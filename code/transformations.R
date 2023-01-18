904cba315065ea32ac7dbabc607dffcefbd2bd32
tem11010 <timothy.e.moore@uconn.edu>
2023-01-18 11:26:10 -0500

    add code folder

---
+++ code/transformations.R
@@ -0,0 +1,12 @@
+# Code for data transformation
+
+# see https://pubmed.ncbi.nlm.nih.gov/15147579/
+# https://www.frontiersin.org/articles/10.3389/fgene.2019.00855/full
+
+# simulate some data using gamma distribution - non-negative, positive skew
+x <- rgamma(10000,shape = 0.4, 1/1000)
+hist(x)
+
+# log2 transformation
+hist(log2(x))
+
