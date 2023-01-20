1913f8a51236c9c64cfb7681994b3b586fec1cd3
tem11010 <timothy.e.moore@uconn.edu>
2023-01-20 12:20:55 -0500

    edit transformation code

--- code/transformations.R
+++ code/transformations.R
@@ -1,7 +1,7 @@
 # Code for data transformation
 
 # see https://pubmed.ncbi.nlm.nih.gov/15147579/
-# https://www.frontiersin.org/articles/10.3389/fgene.2019.00855/full
+# https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4373093/
 
 # simulate some data using gamma distribution - non-negative, positive skew
 x <- rgamma(10000,shape = 0.4, 1/1000)
