e4546c9d7eae9065777bc002831496e169e1cbba
Watt <clw21002work@ad.uconn.edu>
2023-01-25 12:44:12 -0500

    Added file for plotting and comments on package selection.

--- code/filtering.R
+++ code/filtering.R
@@ -1,9 +1,11 @@
 # Code for data filtering
 
-# simulate some data using normal distribution - non-negative, positive skew
+# simulate some data using normal distribution
 x <- rnorm(10000,mean = 0, sd = 5)
 
 hist(x, breaks = 50)
 
 # log2 transformation
 hist(x[x<5], breaks = 50)
+
+
---
+++ code/plotting.R
@@ -0,0 +1,8 @@
+# Code for plotting
+
+# Volcano plot can be created by manipulation of the data and usage of the base
+#function plot() in r
+
+#Alternatively, we could require the ggplot package, which would give us more 
+#flexibility. 
+
