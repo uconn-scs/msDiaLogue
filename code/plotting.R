f8f61a1055603158ba5714234e10768481500b1c
Watt <clw21002work@ad.uconn.edu>
2023-01-27 11:24:41 -0500

    Remove Comments

--- code/plotting.R
+++ code/plotting.R
@@ -1,8 +1,4 @@
 # Code for plotting
 
-# Volcano plot can be created by manipulation of the data and usage of the base
-#function plot() in r
 
-#Alternatively, we could require the ggplot package, which would give us more 
-#flexibility. https://biocorecrg.github.io/CRG_RIntroduction/volcano-plots.html
 
