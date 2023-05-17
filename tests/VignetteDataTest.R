4695a0a5ada353ef6551dd20a085d2f6d4a4409e
Charles Watt <clw21002work@uconn.edu>
2023-05-17 09:28:41 -0400

    Labeling aspects of the VignetteDataTest for use in alpha testing

--- tests/VignetteDataTest.R
+++ tests/VignetteDataTest.R
@@ -1,16 +1,17 @@
 
 name <- "20230510_msDIAlogue HeLa-6mix_Report.csv"
 
-
 data <- preprocessing(name, filterUnique = 3)
-
 
 dataFilter1<- filterOutIn(data, TRUE, c("MYG_HORSE"))
 
 
+
+# Optional 1: generate venn diagram 
 combos.list <- sortcondition(dataFilter1)
 
 vennMain(combos.list)
+#Optional 1
 
 
 
@@ -23,6 +24,8 @@ dataFilter2 <- filterNA(dataImput)
 dataNorm <- normalize(dataFilter2, normalizeType = "Quant")
 
 
+
+# Optional 2: viewing of individual protein path
 dataSetList <- list(
   "Initial" = data, 
   "Transformed" = dataTrans, 
@@ -32,30 +35,37 @@ dataSetList <- list(
 proteinName <- "A0A024R4E5_HUMAN"
 
 A0A024R4E5 <- pullProteinPath(proteinName, dataSetList)
+#Optional 2
+
+
 
 
 
 dataOutput <- summarize(dataNorm, fileName = "")
 
 
-
 compareValues <- c("100fmol","50fmol") 
+
 
 #Volcano Plot
 testOutput1 <- analyze(dataNorm, compareValues, testType = "volcano")
 
 visualize(testOutput1, transformType = "Log 2")
 
+
 #T-Test
 testOutput2 <- analyze(dataNorm, compareValues, testType = "t-test")
+
 
 #MA Plot
 testOutput3 <- analyze(dataNorm, compareValues, testType = "MA")
 
 visualize(testOutput3, graphType = "MA", transformType = "Log 2")
 
+
 #PCA Plot 
 visualize(dataNorm, graphType = "pca")
 
-#heatmap plot
+
+#Heatmap 
 visualize(dataNorm, graphType = "heatmap")
