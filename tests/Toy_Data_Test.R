57473df7d38ef407950402de167356f997f7b09c
Charles Watt <clw21002work@uconn.edu>
2023-06-07 13:00:11 -0400

    June 2023 comments and feedback moved to workflowy

--- tests/Toy_Data_Test.R
+++ tests/Toy_Data_Test.R
@@ -4,11 +4,6 @@ rawData <- read.csv(name)
 
 data <- preprocessing(name, filterUnique = 2, filterNaN = TRUE)
 
-#TODO Getting list csv of protein names to accession number mappings 
-#add csv export of removed data
-
-#add options to use only accessions numbers as protein names
-
 dataFilter1<- filterOutIn(data, TRUE, c("MYG_HORSE"))
 
 
@@ -16,17 +11,11 @@ dataFilter1<- filterOutIn(data, TRUE, c("MYG_HORSE"))
 combos.list <- sortcondition(dataFilter1)
 
 vennMain(combos.list)
-#TODO work on the list of colors that are used for circles
 #Optional 1
 
 
 
-#TODO add regular expression filtering for filterOutIn
-
 dataTrans <- transform(dataFilter1, logFold = 2)
-
-
-#TODO make transformation graphs per protein per condition?
 
 dataImput2 <- impute(dataTrans)
 
@@ -34,27 +23,30 @@ dataFilter2 <- filterNA(dataImput2)
 
 dataNorm <- normalize(dataFilter2, normalizeType = "Mean")
 
+
+
 compareValues <- c("100fmol","50fmol") 
 
 testOutput1 <- analyze(dataNorm, compareValues, testType = "volcano")
 
-
 visualize(testOutput1, conditionLabels = compareValues, transformType = "Log 2")
 
-#TODO add labels for only specified set of proteins are named. 
+
 
 testOutput2 <- analyze(dataNorm, compareValues, testType = "t-test")
 
 visualize(testOutput2, graphType = "t-test",  conditionLabels = compareValues, transformType = "Log 2")
 
-# TODO consider changing breaks  for t-test histograms
+
 
 testOutput3 <- analyze(dataNorm, compareValues, testType = "MA")
 
 visualize(testOutput2, graphType = "MA",  conditionLabels = compareValues, transformType = "Log 2")
 
 
+
 visualize(dataNorm, graphType = "heatmap")
+
 
 
 dataSetList <- list(
