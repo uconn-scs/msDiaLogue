a9702e8d244570bb27e4d4a84950374a86ccc1d0
Carol-seven <542046605@qq.com>
2023-10-20 11:57:10 -0400

    update help_files

--- generate_html_files.R
+++ generate_html_files.R
@@ -1,13 +1,8 @@
 allRd <- list.files("./man")
-for (i in allRd) { 
 
-
-Rd <- file.path(paste0("C:/Users/Charles Watt/Desktop/UCONN RA/msDiaLogue/man/", i))
-
-outfile <- paste0("help_files/", substr(i, 1, nchar(i)-3)  ,".html")
-
-browseURL(Rd2HTML(Rd, outfile, package = "msDiaLogue",
-                  stages = c("install", "render")))
-
+for (i in allRd) {
+  Rd <- file.path(paste0("./man/", i))
+  outfile <- paste0("help_files/", substr(i, 1, nchar(i)-3)  ,".html")
+  browseURL(Rd2HTML(Rd, outfile, package = "msDiaLogue", stages = c("install", "render")))
 }
 
