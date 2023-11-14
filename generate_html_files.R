52109de7526d0618057f6a3e07f7e5830200ca67
Carol-seven <542046605@qq.com>
2023-11-13 23:07:14 -0500

    rm help files

--- generate_html_files.R
+++
@@ -1,8 +0,0 @@
-allRd <- list.files("./man")
-
-for (i in allRd) {
-  Rd <- file.path(paste0("./man/", i))
-  outfile <- paste0("help_files/", substr(i, 1, nchar(i)-3)  ,".html")
-  browseURL(Rd2HTML(Rd, outfile, package = "msDiaLogue", stages = c("install", "render")))
-}
-
