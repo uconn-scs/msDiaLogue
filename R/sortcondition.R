3333381752f8ae8d08b2ef0ef45bcb2a6692ce6c
Carol-seven <542046605@qq.com>
2023-11-18 01:27:52 -0500

    sortcondition: rewrite

--- R/sortcondition.R
+++ R/sortcondition.R
@@ -11,7 +11,7 @@
 #' @param dataSet The 2d data set of experimental values.
 #' 
 #' @import dplyr
-#' @import tidyr
+#' @importFrom tibble column_to_rownames
 #' 
 #' @returns A list of lists.
 #' 
@@ -19,36 +19,20 @@
 
 sortcondition <- function(dataSet) {
   
-  ## create a list of the unique condition names
-  conditionList <- unique(dataSet$R.Condition)
-  
   ## calculate how many values are present from each condition by protein
-  dtaCounts <- dataSet %>%
+  dataCounts <- dataSet %>%
     ## do not include columns that are strings
     select(!c(R.FileName, R.Replicate)) %>%
     ## group by experimental condition
     group_by(R.Condition) %>%
     ## apply to every numeric function a summation of the non-NA values
-    summarise(across(where(is.numeric), ~sum(!is.na(.x))))
+    summarise(across(where(is.numeric), ~sum(!is.na(.x)))) %>%
+    column_to_rownames("R.Condition")
   
-  ## initialize a list to populate with the keyed dictionary entries
-  conditionProteinDict <- list()
-  
-  ## go through each condition in the experiment
-  for (condition in conditionList) {
-    
-    ## select only the data from this condition
-    dtaTmp <- dtaCounts[dtaCounts$R.Condition == condition, -1]
-    
-    ## find the columns that have values greater than 0
-    dtaTmp <- dtaTmp[which(dtaTmp > 0)]
-    
-    ## create a temporary list of the column names that are remained
-    listTmp <- as.list(names(dtaTmp))
-    
-    ## assign the temporary list to the condition name in the output list
-    conditionProteinDict [[toString(condition)]] <- listTmp
-  }
+  ## create the keyed dictionary
+  conditionProteinDict <- setNames(apply(dataCounts, 1, function(row) {
+    names(row[row > 0])
+  }, simplify = FALSE), rownames(dataCounts))
   
   ## return the keyed dictionary
   return(conditionProteinDict)
