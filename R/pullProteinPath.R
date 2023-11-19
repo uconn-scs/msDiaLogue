8a2791707c2e482d1488e0ef1ce3424d713a7518
Carol-seven <542046605@qq.com>
2023-11-19 14:21:11 -0500

    pullProteinPath: rewrite function; create test

--- R/pullProteinPath.R
+++ R/pullProteinPath.R
@@ -15,24 +15,8 @@
 
 pullProteinPath <- function(proteinName, dataSetList) {
   
-  ## label the data frame
-  proteinPath <- dataSetList[[1]][1:3]
-  
-  ## for each provided data set
-  for (i in 1:length(dataSetList)) {
-    
-    ## pull out the name of the type of data
-    dataLabel <- names(dataSetList)[i]
-    
-    ## select the correct protein data
-    selectTemp <- dataSetList[[i]] %>% select(proteinName)
-    
-    ## label the correct protein data with the data type
-    names(selectTemp) <- dataLabel
-    
-    ## add the labeled data to the data frame
-    proteinPath <- cbind(proteinPath, selectTemp)
-  }
+  proteinPath <- cbind(dataSetList[[1]][c("R.Condition", "R.FileName", "R.Replicate")],
+                       sapply(dataSetList, function(mat) mat[[proteinName]]))
   
   ## return the path of data for this protein
   return(proteinPath)
---
+++ tests/testthat/test-pullProteinPath.R
@@ -0,0 +1,25 @@
+
+test_that("pullProteinPath", {
+  
+  ## load data from previous step in work flow
+  dataSet <- read.csv("../storedData/preprocessing_Toy.csv")
+  dataTran <- read.csv("../storedData/transform_Toy.csv")
+  dataNorm <- read.csv("../storedData/normalize_quant_Toy.csv")
+  dataImput <- read.csv("../storedData/impute_Toy.csv")
+  
+  ## execute current function 'pullProteinPath' on data file
+  invisible(capture.output(
+    data <- pullProteinPath(proteinName = "ZC11B_HUMAN",
+                            dataSetList = list(Initial = dataSet,
+                                               Transformed = dataTran,
+                                               Normalized = dataNorm,
+                                               Imputed = dataImput))
+  ))
+  
+  ## load stored correct data
+  storedData <- read.csv("../storedData/pullProteinPath_Toy.csv")
+  
+  ## test if current function yields equal results to previous version
+  expect_equal(data, storedData)
+  
+})
