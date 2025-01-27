b6c2afad473c73b836aac31fe9cd575c89563c4c
Carol-seven <542046605@qq.com>
2025-01-27 17:57:42 -0500

    add root transformation to transform

--- R/transform.R
+++ R/transform.R
@@ -69,20 +69,32 @@ meanVarPlot <- function(datMV, title = "") {
 
 ##----------------------------------------------------------------------------------------
 #' 
-#' Log-based transformation
+#' Transformation
 #' 
 #' @description
-#' Apply a logarithmic transformation to the data to stabilize the variance.
+#' Apply a transformation to the data to stabilize the variance.
 #'  
 #' @param dataSet A data frame containing the data signals.
+#' 
+#' @param method A string (default = "log") specifying the method to be used for the
+#' transformation:
+#' \enumerate{
+#' \item "log": Logarithm transformation.
+#' \item "root": Root transformation.
+#' }
 #'  
-#' @param logFold An integer specifying the base for the log transformation.
+#' @param logFold An integer (default = 2) specifying the base for the log transformation
+#' when \code{method = "log"}.
+#' 
+#' @param root An integer (default = 2) specifying the degree of the root for the root
+#' transformation when \code{method = "root"}. For example, set it to 2 for square root or
+#' 3 for cube root.
 #' 
 #' @details
 #' The function executes the following:
 #' \enumerate{
-#' \item Plots the mean-variance relationship using \code{meanVariancePlot()}.
-#' \item Log-transforms the data, using the specified base.
+#' \item Plots the mean-variance relationship.
+#' \item Transforms the data.
 #' \item Plots the mean-variance relationship again for comparison.
 #' }
 #' 
@@ -92,7 +104,7 @@ meanVarPlot <- function(datMV, title = "") {
 #' 
 #' @export
 
-transform <- function(dataSet, logFold = 2) {
+transform <- function(dataSet, method = "log", logFold = 2, root = 2) {
   
   ## organize the data for transformation
   dataPoints <- dataSet %>%
@@ -103,7 +115,11 @@ transform <- function(dataSet, logFold = 2) {
   print(plotPre)
   
   ## take the log of the numerical data
-  transDataPoints <- log(dataPoints, logFold)
+  if (method == "log") {
+    transDataPoints <- log(dataPoints, logFold)
+  } else if (method == "root") {
+    transDataPoints <- dataPoints^(1/root)
+  }
   
   ## calculate and plot a mean-variance plot 
   plotPost <- meanVarPlot(transDataPoints,  title = "Post-Transformation")
