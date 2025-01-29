536d790d0d859cb5250f679456f2e26ec4187283
Carol-seven <542046605@qq.com>
2025-01-29 15:50:28 -0500

    add scaling and centering to normalize

--- R/normalize.R
+++ R/normalize.R
@@ -27,13 +27,21 @@
 #' 
 #' @param dataSet The 2d data set of experimental values.
 #' 
+#' @param applyto A string (default = "sample") specifying the target of normalization.
+#' Options are "sample" or "row" (across rows), and "protein" or "column" (across columns).
+#' 
 #' @param normalizeType A string (default = "quant") specifying which type of
 #' normalization to apply:
-#' \enumerate{
-#' \item "quant": Quantile \insertCite{bolstad2003comparison}{msDiaLogue}
-#' \item "median": Protein-wise Median
-#' \item "mean": Protein-wise Mean
-#' \item "none": None
+#' \itemize{
+#' \item "auto": Auto scaling.
+#' \item "level": Level scaling.
+#' \item "mean": Mean centering.
+#' \item "median": Median centering.
+#' \item "pareto": Pareto scaling.
+#' \item "quant": Quantile normalization \insertCite{bolstad2003comparison}{msDiaLogue}.
+#' \item "range": Range scaling.
+#' \item "vast": Vast scaling.
+#' \item "none": None.
 #' }
 #' 
 #' @details
@@ -56,41 +64,93 @@
 #' 
 #' @export
 
-normalize <- function(dataSet, normalizeType = "quant") {
+normalize <- function(dataSet, applyto = "sample", normalizeType = "quant") {
   
   ## create a boxplot for pre-normalization
   visualize(dataSet, graphType = "normalize") +
     ggtitle("Pre-Normalization Boxplot")
   
   ## select the numerical data
-  dataPoints <- t(select(dataSet, -c(R.Condition, R.Replicate)))
+  if (applyto == "sample") {
+    dataPoints <- t(select(dataSet, -c(R.Condition, R.Replicate)))
+  } else if (applyto == "protein") {
+    dataPoints <- select(dataSet, -c(R.Condition, R.Replicate))
+  }
   
-  if (normalizeType == "quant") {
+  if (normalizeType == "auto") {
     
-    normDataPoints <- limma::normalizeQuantiles(dataPoints)
+    ## auto scaling
+    normDataPoints <- base::scale(dataPoints, center = TRUE, scale = TRUE)
     
-    ## subtracts the median intensity of each protein from every value for that protein
+  } else if (normalizeType == "level") {
+    
+    ## level scaling
+    normDataPoints <- base::scale(dataPoints, center = TRUE,
+                                  scale = apply(dataPoints, 2, mean, na.rm = TRUE))
+    
+  } else if (normalizeType == "mean") {
+    
+    ## mean centering
+    normDataPoints <- scale(dataPoints, center = TRUE, scale = FALSE)
+    
   } else if (normalizeType == "median") {
+    
+    ## median centering
     normDataPoints <- scale(dataPoints,
                             center = apply(dataPoints, 2, median, na.rm = TRUE),
                             scale = FALSE)
     
-    ## subtracts the average intensity of each protein from every value for that protein
-  } else if (normalizeType == "mean") {
-    normDataPoints <- scale(dataPoints, center = TRUE, scale = FALSE)
+  } else if (normalizeType == "pareto") {
     
-    ## throws a warning for missing normalization, which may introduce errors
+    ## Pareto scaling
+    sigma <- apply(base::scale(dataPoints, center = TRUE, scale = FALSE), 2, function(col) {
+      col <- col[!is.na(col)]
+      sqrt(sum(col^2) / max(1L, length(col)-1L))
+    })
+    normDataPoints <- base::scale(dataPoints, center = TRUE, scale = sqrt(sigma))
+    
+  } else if (normalizeType == "quant") {
+    
+    ## quantile normalization
+    normDataPoints <- limma::normalizeQuantiles(dataPoints)
+    
+  } else if (normalizeType == "range") {
+    
+    ## range scaling
+    range_diff <- apply(dataPoints, 2, function(col) {
+      res <- max(col, na.rm = TRUE) - min(col, na.rm = TRUE)
+      res <- ifelse(res == 0, 1, res)
+      return(res)
+    })
+    normDataPoints <- base::scale(dataPoints, center = TRUE, scale = range_diff)
+    
+  } else if (normalizeType == "vast") {
+    
+    ## vast scaling
+    mu <- colMeans(dataPoints, na.rm = TRUE)
+    sigma <- apply(base::scale(dataPoints, center = TRUE, scale = FALSE), 2, function(col) {
+      col <- col[!is.na(col)]
+      sqrt(sum(col^2) / max(1L, length(col)-1L))
+    })
+    normDataPoints <- base::scale(dataPoints, center = TRUE, scale = sigma^2/mu)
+    
   } else if (normalizeType == "none") {
+    
     warning("You are currently choosing NOT to normalize your data.
             This is heavily discouraged in most scientific work.
             Please be confident this is the correct choice.")
-    
     normDataPoints <- dataPoints
+    
   }
   
   ## recombine the labels and transformed data into a single data frame
-  normDataSet <- cbind(dataSet[,c("R.Condition", "R.Replicate")],
-                       t(normDataPoints))
+  if (applyto == "sample") {
+    normDataSet <- cbind(dataSet[,c("R.Condition", "R.Replicate")],
+                         t(normDataPoints))
+  } else if (applyto == "protein") {
+    normDataSet <- cbind(dataSet[,c("R.Condition", "R.Replicate")],
+                         normDataPoints)
+  }
   
   ## replace the protein names
   colnames(normDataSet) <- colnames(dataSet)
