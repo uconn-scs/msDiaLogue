381ff40675eb91fcf592e2e03176280e7b63444e
Carol-seven <542046605@qq.com>
2026-02-27 23:59:31 -0500

    refotmat filtering

--- R/filtering.R
+++ R/filtering.R
@@ -1,49 +1,29 @@
-##
-## msDiaLogue: Analysis + Visuals for Data Indep. Aquisition Mass Spectrometry Data
-## Copyright (C) 2025  Shiying Xiao, Timothy Moore and Charles Watt
-## Shiying Xiao <shiying.xiao@uconn.edu>
-##
-## This file is part of the R package msDiaLogue.
-##
-## The R package msDiaLogue is free software: You can redistribute it and/or
-## modify it under the terms of the GNU General Public License as published by
-## the Free Software Foundation, either version 3 of the License, or any later
-## version (at your option). See the GNU General Public License at
-## <https://www.gnu.org/licenses/> for details.
-##
-## The R package msDiaLogue is distributed in the hope that it will be useful,
-## but WITHOUT ANY WARRANTY without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-##
-
-#################################
-#### Code for data filtering ####
-#################################
+###################
+#### Filtering ####
+###################
 #'
 #' Filtering of raw data signals
 #' 
 #' @description
 #' Apply a series of filtering steps to the data set.
 #' 
-#' @param dataSet The 2d data set of experimental values.
+#' @param dataSet A data frame containing the data signals.
 #' 
-#' @param filterNaN A boolean (default = TRUE) specifying whether proteins with NaN should
-#' be removed from the data set.
+#' @param filterNaN A logical value (default = TRUE) specifying whether
+#' proteins with NaN should be removed from the data set.
 #' 
-#' @param filterUnique An integer (default = 2) specifying whether proteins with less than
-#' the default number of unique peptides should be removed from the data set.
+#' @param filterUnique An integer (default = 2) specifying whether
+#' proteins with less than the default number of unique peptides should be
+#' removed from the data set.
 #' 
-#' @param replaceBlank A boolean (default = TRUE) specifying whether proteins without names
-#' should be be named by their accession numbers.
+#' @param replaceBlank A logical value (default = TRUE) specifying whether
+#' proteins without names should be be named by their accession numbers.
 #' 
-#' @param saveRm A boolean (default = TRUE) specifying whether to save removed data to
-#' current working directory.
-#' 
-#' @details
-#' All forms of filtering are recommended for most use cases.
+#' @param saveRm A logical value (default = TRUE) specifying whether
+#' to save removed data to current working directory.
 #' 
 #' @return
-#' A filtered 2d dataframe.
+#' A filtered data frame.
 #' 
 #' @autoglobal
 #' 
@@ -104,48 +84,50 @@ preProcessFiltering <- function(dataSet,
   return(filteredData)
 }
 
-
-##----------------------------------------------------------------------------------------
-#' 
-#' Filtering proteins or contaminants
+##------------------------------------------------------------------------------
+#'
+#' Filter proteins by list or pattern
 #' 
 #' @description
 #' Apply a series of filtering steps to the data set.
 #' 
-#' @param dataSet The 2d data set of experimental values.
+#' @param dataSet A data frame containing the data signals.
 #' 
-#' @param listName A character vector of text strings used as keys for selecting or
-#' removing.
+#' @param listName A character vector of text strings used as keys for
+#' selecting or removing.
 #' 
-#' @param regexName A character vector specifying proteins for regular expression pattern
-#' matching to select or remove.
+#' @param regexName A character vector specifying proteins for regular
+#' expression pattern matching to select or remove.
 #' 
-#' @param by A character string (default = "PG.ProteinName" for Spectronaut, default =
-#' "AccessionNumber" for Scaffold) specifying the information to which \code{listName}
-#' and/or \code{regexName} filter is applied. Allowable options include:
+#' @param by A character string (default = "PG.ProteinName" for Spectronaut,
+#' default = "AccessionNumber" for Scaffold) specifying the information to
+#' which \code{listName} and/or \code{regexName} filter is applied.
+#' Allowable options include:
 #' \itemize{
-#' \item For Spectronaut: "PG.Genes", "PG.ProteinAccession", "PG.ProteinDescriptions", and
-#' "PG.ProteinName".
-#' \item For Scaffold: "ProteinDescriptions", "AccessionNumber", and "AlternateID".
+#' \item For Spectronaut: "PG.Genes", "PG.ProteinAccession",
+#' "PG.ProteinDescriptions", and "PG.ProteinName".
+#' \item For Scaffold: "ProteinDescriptions", "AccessionNumber", and
+#' "AlternateID".
 #' }
 #' 
-#' @param removeList A boolean (default = TRUE) specifying whether the list of proteins
-#' should be removed or selected.
+#' @param removeList A logical value (default = TRUE) specifying whether
+#' the list of proteins should be removed or selected.
 #' \itemize{
-#' \item TRUE: Remove the list of proteins from the data set. 
+#' \item TRUE: Remove the list of proteins from the data set.
 #' \item FALSE: Remove all proteins not in the list from the data set.
 #' }
 #' 
-#' @param saveRm A boolean (default = TRUE) specifying whether to save removed data to
-#' current working directory. This option only works when \code{removeList = TRUE}.
-#' 
-#' @details
-#' If both \code{listName} and \code{regexName} are provided, the proteins selected or
-#' removed will be the union of those specified in \code{listName} and those matching
-#' the regex pattern in \code{regexName}.
+#' @param saveRm A logical value (default = TRUE) specifying whether
+#' to save removed data to current working directory.
+#' This option only works when \code{removeList = TRUE}.
 #' 
 #' @return
-#' A filtered 2d dataframe.
+#' A filtered data frame.
+#' 
+#' @details
+#' If both \code{listName} and \code{regexName} are provided, the proteins
+#' selected or removed will be the union of those specified in \code{listName}
+#' and those matching the regex pattern in \code{regexName}.
 #' 
 #' @export
 
@@ -216,37 +198,33 @@ filterOutIn <- function(dataSet,
   return(filteredData)
 }
 
-
-##----------------------------------------------------------------------------------------
+##------------------------------------------------------------------------------
 #' 
-#' Filtering NA's post-imputation
+#' Filter proteins by non-missing proportion
 #' 
 #' @description
 #' Remove proteins with NA values.
 #' 
 #' @param dataSet The 2d data set of experimental values.
 #' 
-#' @param minProp A scalar (default = 0.51) specifying the minimum non-missing
-#' proportion required for a protein to be retained.
+#' @param minProp A numeric value (default = 0.51) specifying the minimum
+#' non-missing proportion required for a protein to be retained.
 #' 
-#' @param by A string (default = "cond") specifying how coverage is evaluated.
+#' @param by A character string (default = "cond") specifying
+#' how coverage is evaluated.
 #' \itemize{
-#' \item \code{"cond"}: The non-missing proportion for a protein must be at
-#' least \code{minProp} within each condition. Proteins failing in any condition
-#' are filtered out. 
+#' \item \code{"cond"}: The non-missing proportion for a protein must be
+#' at least \code{minProp} within each condition. Proteins failing in any
+#' condition are filtered out. 
 #' \item \code{"all"}: The overall non-missing proportion across all samples
 #' must be at least \code{minProp}.
 #' }
 #' 
-#' @param saveRm A boolean (default = TRUE) specifying whether to save removed data to
-#' current working directory.
-#' 
-#' @details 
-#' If proteins that do not meet the imputation requirement are removed, a .csv file is
-#' created with the removed data.
+#' @param saveRm A logical value (default = TRUE) specifying whether
+#' to save removed data to current working directory.
 #' 
 #' @return
-#' A filtered 2d dataframe.
+#' A filtered data frame.
 #' 
 #' @export
 
