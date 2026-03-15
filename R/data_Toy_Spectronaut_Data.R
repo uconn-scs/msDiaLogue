b4e4e113b0c92eb8549ceb6a3176ca787c999643
Carol-seven <542046605@qq.com>
2026-03-15 11:41:58 -0400

    add example data

---
+++ R/data_Toy_Spectronaut_Data.R
@@ -0,0 +1,14 @@
+#' Toy Spectronaut Protein Abundance Dataset
+#' 
+#' @description
+#' A small toy dataset exported from Spectronaut. The data structure mimics
+#' the output format of Spectronaut protein reports and can be used to
+#' demonstrate the workflow of the \pkg{msDiaLogue} package.
+#'
+#' @format
+#' A data frame with 672 rows and 16 columns.
+#' 
+#' @source
+#' \href{https://proteomics.uconn.edu/}{Proteomics & Metabolomics Facility, University of Connecticut}
+
+"Toy_Spectronaut_Data"
