e00125eda5c0c8a73131519aacdb18502f1da44f
Carol-seven <542046605@qq.com>
2026-03-22 01:55:58 -0400

    add Scaffold example data

---
+++ R/data.R
@@ -0,0 +1,36 @@
+##############
+#### Data ####
+##############
+#'
+#' Toy Spectronaut Protein Abundance Dataset
+#' 
+#' @description
+#' A small toy dataset exported from Spectronaut. The data structure mimics
+#' the output format of Spectronaut protein reports and can be used to
+#' demonstrate the workflow of the \pkg{msDiaLogue} package.
+#'
+#' @format
+#' A data frame with 672 rows and 16 columns.
+#' 
+#' @source
+#' \href{https://proteomics.uconn.edu/}{Proteomics & Metabolomics Facility, University of Connecticut}
+
+"Toy_Spectronaut_Data"
+
+##------------------------------------------------------------------------------
+#' 
+#' Toy Scaffold Protein Abundance Dataset
+#' 
+#' @description
+#' A small toy dataset exported from Scaffold. The data structure mimics
+#' the output format of Scaffold protein reports and can be used to
+#' demonstrate the workflow of the \pkg{msDiaLogue} package.
+#'
+#' @format
+#' A data frame with 1737 rows and 20 columns.
+#' 
+#' @source
+#' \href{https://proteomics.uconn.edu/}{Proteomics & Metabolomics Facility, University of Connecticut}
+
+"Toy_Scaffold_Data"
+
